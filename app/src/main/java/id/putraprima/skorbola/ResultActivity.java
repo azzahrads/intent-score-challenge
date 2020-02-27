e16608cc4bb53892d6981b8750e338a96ef21315
Azzahra Dinda <azzahradindas@gmail.com>
2020-02-27 10:10:16 +0700

    Update - Add scorer with name

--- app/src/main/java/id/putraprima/skorbola/ResultActivity.java
+++ app/src/main/java/id/putraprima/skorbola/ResultActivity.java
@@ -3,12 +3,47 @@ package id.putraprima.skorbola;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import id.putraprima.skorbola.model.Match;
 
 public class ResultActivity extends AppCompatActivity {
+
+    private static final String DATA_KEY = "data";
+    TextView tvWinner, tvList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+        tvWinner = findViewById(R.id.tv_winner);
+        tvList = findViewById(R.id.tv_list);
+
+        Bundle extras = getIntent().getExtras();
+        Match match = getIntent().getParcelableExtra(DATA_KEY);
+        if (extras != null) {
+            String name = "";
+            if(match.resultScore().equals(match.getHomeTeam())) {
+                for (String win : match.getHomeScorer()) {
+                    name += win + "\n";
+                    Log.d("who?", "list of scorer " + win);
+                }
+                tvWinner.setText("The winner is "+ match.resultScore());
+                tvList.setText(name);
+            }else if(match.resultScore().equals(match.getAwayTeam())) {
+                for (String win : match.getAwayScorer()) {
+                    name += win + "\n";
+                    Log.d("who?", "list of scorer " + win);
+                }
+                tvWinner.setText("The winner is "+ match.resultScore());
+                tvList.setText(name);
+            }else{
+                tvWinner.setText(match.resultScore());
+                tvList.setText("");
+            }
+        }
     }
 }
